87e02bc2f473f2285348f1d5937c96c896cbb5d7
Roman Rudavskiy <noteR@macbook-pro-roman.local>
2016-06-21 17:01:51 +0300

    Initial Commit

---
+++ pokepedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokepedia
+//
+//  Created by Roman on 21.06.16.
+//  Copyright © 2016 Roman.Rudavskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
