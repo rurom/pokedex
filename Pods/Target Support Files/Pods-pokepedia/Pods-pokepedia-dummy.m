720f1ba8b6998b1922a1cf57eac8e8166189c7a9
Roman <noteR@MacBook-Pro-Roman.local>
2016-11-26 17:00:52 +0200

    Added Cocoapods+Alamofire

---
+++ Pods/Target Support Files/Pods-pokepedia/Pods-pokepedia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pokepedia : NSObject
+@end
+@implementation PodsDummy_Pods_pokepedia
+@end
---
+++ Pods/Target Support Files/Pods-pokepedia/Pods-pokepedia-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_pokepediaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_pokepediaVersionString[];
+
